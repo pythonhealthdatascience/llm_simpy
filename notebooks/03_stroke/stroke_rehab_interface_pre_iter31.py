cac151506141e269827774a6d52ebdd722e15cd5
TomMonks <thomas.m.monks@gmail.com>
2024-06-25 16:31:29 +0100

    fix(iter31): dynamic sized charts

--- notebooks/03_stroke/stroke_rehab_interface_pre_iter31.py
+++ notebooks/03_stroke/stroke_rehab_interface_pre_iter31.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from stroke_rehab_model import (
+from stroke_rehab_model_fix import (
     Experiment, multiple_replications, combine_pdelay_results, 
     combine_occup_results, mean_results, prob_delay_plot, 
     occupancy_plot, summary_table
@@ -66,17 +66,17 @@ def main():
 
         # Display plots
         st.subheader("Probability Delay Plots")
-        fig_pd_asu, ax_pd_asu = prob_delay_plot(mean_pd_asu, np.arange(len(mean_pd_asu)))
+        fig_pd_asu, ax_pd_asu = prob_delay_plot(mean_pd_asu, np.arange(0, len(mean_pd_asu)))
         st.pyplot(fig_pd_asu)
 
-        fig_pd_rehab, ax_pd_rehab = prob_delay_plot(mean_pd_rehab, np.arange(len(mean_pd_rehab)), "No. rehab beds available")
+        fig_pd_rehab, ax_pd_rehab = prob_delay_plot(mean_pd_rehab, np.arange(0, len(mean_pd_rehab)), "No. rehab beds available")
         st.pyplot(fig_pd_rehab)
 
         st.subheader("Occupancy Plots")
-        fig_occ_asu, ax_occ_asu = occupancy_plot(mean_rel_asu, np.arange(len(mean_rel_asu)))
+        fig_occ_asu, ax_occ_asu = occupancy_plot(mean_rel_asu, np.arange(0, len(mean_rel_asu)))
         st.pyplot(fig_occ_asu)
 
-        fig_occ_rehab, ax_occ_rehab = occupancy_plot(mean_rel_rehab, np.arange(len(mean_rel_rehab)), "No. people in rehab")
+        fig_occ_rehab, ax_occ_rehab = occupancy_plot(mean_rel_rehab, np.arange(0, len(mean_rel_rehab)), "No. people in rehab")
         st.pyplot(fig_occ_rehab)
 
 if __name__ == "__main__":
